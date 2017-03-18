e6217c239f79a58971b1c3ab3c89e81d64c65971
Emilie <emilieb2r@gmail.com>
2017-03-18 11:42:21 +0100

    site

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bug7)w%zkwpsol@5bx!w9-m2pjeu-rw63vbz3@hix*gj_*^2a$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["emilieb2r.pythonanywhere.com"]
 
 
 # Application definition
@@ -119,5 +119,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
-    
