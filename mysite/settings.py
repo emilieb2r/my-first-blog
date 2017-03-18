e3bdae62d1ee84935e15967f369aa919f23fbadb
Emilie <emilieb2r@gmail.com>
2017-03-18 15:57:39 +0100

    Modif templates to display posts from database

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bug7)w%zkwpsol@5bx!w9-m2pjeu-rw63vbz3@hix*gj_*^2a$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['emilieb2r.pythonanywhere.com']
+ALLOWED_HOSTS = ['emilieb2r.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
